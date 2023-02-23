545cc446fd917611162ca6d60f6aee129dce2f1c
justdoitjun <119689162+justdoitjun@users.noreply.github.com>
2023-02-23 17:24:49 +0900

    Update P73.java
    update for understanding GitHub

--- day02/src/variable/P73.java
+++ day02/src/variable/P73.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class P73 {
 
 	public static void main(String[] args) {
+		String str = "";
 		Scanner sc = new Scanner(System.in);
 		System.out.println("명령어를 입력하시오(q,i,d,s)");
 		String cmd = sc.next();
