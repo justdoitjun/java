b8d7bb96343477ffca5f0703202e7335ca31d30c
justdoitjun <bing74700@gmail.com>
2023-02-23 09:36:48 +0900

    DAY02LECTURE

---
+++ day02/src/day02/P55.java
@@ -0,0 +1,16 @@
+package day02;
+
+public class P55 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 2_000_000_000;
+		int b = 2_000_000_000;
+		
+			
+		long c = a;
+		long d = b;
+		System.out.println(c + d);
+	}
+
+}
