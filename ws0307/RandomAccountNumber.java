0ef4e97e914b27ce504857cf48cfeb2965091f85
justdoitjun <119689162+justdoitjun@users.noreply.github.com>
2023-03-07 17:47:16 +0900

    JunhyeokChoi_Assignment_ThirdUpdate_03_07_2023
    wrong package name.

---
+++ ws0307/RandomAccountNumber.java
@@ -0,0 +1,18 @@
+package ws0307;
+
+import java.util.Random;
+
+public class RandomAccountNumber {// 아직까지는 쓰지는 않는 클래스임. 강의 막판에 해보다가 안되어서 둠. 다음에 할 것.
+	
+	public RandomAccountNumber() {
+	}
+
+	public static String make() {
+		String accPiece = "";
+		Random r = new Random(10);
+		accPiece = r + "";
+//		accPiece = r.toString();
+		return accPiece;
+	}
+
+}
